db44da1c38d16c87028cd1fa4e16488ee34e7a66
Brett Smith <br.smith@draftkings.com>
2023-06-02 18:30:29 -0500

    Added fix for omnisharp

--- lua/abi/remap.lua
+++ lua/abi/remap.lua
@@ -1,2 +1,8 @@
 vim.g.mapleader = " "
+
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action)
+vim.keymap.set("n", "<C-s>", vim.cmd.w)
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+
+
